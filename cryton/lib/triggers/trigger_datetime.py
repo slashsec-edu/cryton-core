01ad57f046830a75022c836dde2944277fabad62
Milan Boháček <244656@mail.muni.cz>
2021-12-30 15:05:39 +0100

    Resolve "DateTime trigger fix"

--- cryton/lib/triggers/trigger_datetime.py
+++ cryton/lib/triggers/trigger_datetime.py
@@ -1,17 +1,16 @@
 from datetime import datetime
-from schema import Schema, Optional, And, Or, Use
-from pytz import timezone, all_timezones
+from schema import Schema, Optional, And, Or
+import pytz
 
+from cryton.lib.util import util
 from cryton.lib.triggers.trigger_base import TriggerTime
 
 
 class TriggerDateTime(TriggerTime):
     arg_schema = Schema({
         Or('year', 'month', 'day', 'hour', 'minute', 'second', only_one=False): int,
-        # validates that the supplied timezone is in pytz timezones in lowercase,
-        # because pytz.timezone() function is not case sensitive
-        Optional('timezone'): And(lambda x: x.lower() in map(lambda y: y.lower(), all_timezones),
-                                  error='Invalid timezone')
+        # validates that the supplied timezone is in pytz timezones
+        Optional('timezone'): And(lambda x: x in pytz.all_timezones, error='Invalid timezone')
         })
 
     def __init__(self, stage_execution):
@@ -25,16 +24,21 @@ class TriggerDateTime(TriggerTime):
         Create Stage's start time.
         :return: Stage's start time
         """
-        today = datetime.utcnow()
-        trigger_args = self.stage_execution.model.stage_model.trigger_args
-        args_timezone = trigger_args.get('timezone')
 
-        if args_timezone is not None and args_timezone.lower() != "utc":
-            today = today.astimezone(timezone(args_timezone))
+        trigger_args = self.stage_execution.model.stage_model.trigger_args
+        args_timezone = trigger_args.get('timezone', 'UTC')
+
+        if args_timezone is None:
+            today = datetime.utcnow()
+        else:
+            today = datetime.now(pytz.timezone(args_timezone))
 
         schedule_datetime = datetime(year=trigger_args.get('year', today.year),
                                      month=trigger_args.get('month', today.month),
                                      day=trigger_args.get('day', today.day), hour=trigger_args.get('hour', 00),
                                      minute=trigger_args.get('minute', 00), second=trigger_args.get('second', 00))
+
+        if schedule_datetime.tzinfo != pytz.utc:
+            schedule_datetime = util.convert_to_utc(schedule_datetime, args_timezone, True)
 
         return schedule_datetime
